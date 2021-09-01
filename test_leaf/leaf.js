95c50c2f4494ac77b2adc4fe0656cfadf2b5d25a
Joaquim Breno <62159887+JoaquimBreno@users.noreply.github.com>
2021-09-01 16:13:48 -0300

    LeafletRed Co-authored-by: Guilherme Jacome Cavalcante <GuilhermeJC13@users.noreply.github.com>

--- test_leaf/leaf.js
+++ test_leaf/leaf.js
@@ -1,4 +1,6 @@
 var map = L.map('map').setView([-7.1311923, -36.8275259],7);
+map.createPane('labels');
+
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
@@ -7,4 +9,51 @@ var map = L.map('map').setView([-7.1311923, -36.8275259],7);
 		zoomOffset: -1
 	}).addTo(map);
 
-var geojson = L.geoJson(statesData).addTo(map);
+
+    function style(feature) {
+        return {
+            fillColor: "#F33333",
+            weight: 1,
+            opacity: 0.5,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 1
+        };
+    }
+
+    function highlightFeature(e) {
+        var layer = e.target;
+    
+        layer.setStyle({
+            weight: 5,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+    
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+    }
+
+    function resetHighlight(e) {
+        geojson.resetStyle(e.target);
+    }
+
+    function zoomToFeature(e) {
+        map.fitBounds(e.target.getBounds());
+    }
+
+    function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+            click: zoomToFeature
+        });
+    }
+    
+// var geojson = L.geoJson(statesData).addTo(map);
+var geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
