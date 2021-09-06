3ef84101a714a46a695437848ea80896cc318f99
Joaquim Breno <62159887+JoaquimBreno@users.noreply.github.com>
2021-09-06 18:33:37 -0300

    Info Updated
    Co-authored-by: Guilherme Jacome Cavalcante <GuilhermeJC13@users.noreply.github.com>

--- test_leaf/leaf.js
+++ test_leaf/leaf.js
@@ -1,59 +1,81 @@
 var map = L.map('map').setView([-7.1311923, -36.8275259],7);
-map.createPane('labels');
 
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    id: 'mapbox/light-v9',
+    tileSize: 512,
+    zoomOffset: -1,
+    maxZoom: 10,
+    minZoom: 6
+}).addTo(map);
 
-	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		id: 'mapbox/light-v9',
-		tileSize: 512,
-		zoomOffset: -1
-	}).addTo(map);
+map.setMaxBounds(map.getBounds());
 
+var info = L.control();
 
-    function style(feature) {
-        return {
-            fillColor: "#F33333",
-            weight: 1,
-            opacity: 0.5,
-            color: 'white',
-            dashArray: '3',
-            fillOpacity: 1
-        };
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Município da Paraíba</h4>' +  (props ?
+        '<b>' + props.NM_MUNICIP + '</b><br />'
+        : 'Selecione o município');
+};
+
+function style(feature) {
+    return {
+        fillColor: "#F33333",
+        weight: 1,
+        opacity: 0.5,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 1
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    info.update(layer.feature.properties);
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
     }
+}
 
-    function highlightFeature(e) {
-        var layer = e.target;
-    
-        layer.setStyle({
-            weight: 5,
-            color: '#666',
-            dashArray: '',
-            fillOpacity: 0.7
-        });
-    
-        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-            layer.bringToFront();
-        }
-    }
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
 
-    function resetHighlight(e) {
-        geojson.resetStyle(e.target);
-    }
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
 
-    function zoomToFeature(e) {
-        map.fitBounds(e.target.getBounds());
-    }
-
-    function onEachFeature(feature, layer) {
-        layer.on({
-            mouseover: highlightFeature,
-            mouseout: resetHighlight,
-            click: zoomToFeature
-        });
-    }
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
     
 // var geojson = L.geoJson(statesData).addTo(map);
 var geojson = L.geoJson(statesData, {
     style: style,
     onEachFeature: onEachFeature
 }).addTo(map);
+
+
+info.addTo(map);
